d8656f2c48393984f8d0a89ae8c6db3468838cc5
zach.jung <zach.jung@kakaocorp.com>
2017-01-07 17:53:43 +0900

    this.props로 내려받도록 구조 바꿈

--- src/components/Modify.jsx
+++ src/components/Modify.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { render } from 'react-dom';
+import ModifyForm from './ModifyForm.jsx'
 
 let {
     form,
@@ -18,27 +18,22 @@ class Modify extends Component {
 
     componentDidMount() {
         console.log("!!!!!!!!!!!!");
-        fetch("http://localhost:5001/editJournal?id=587064712736323e86d0a423", {method: "post"})
+        fetch("http://localhost:3000/editJournal?id=587064712736323e86d0a423", {method: "post"})
             .then(response => response.json())
             .then(json => {
                 console.log(json);
-                this.setState({
-                    data: json,
-                });
+                this.setState({...this.state, data: json});
             });
     }
 
     render(){
-        console.log(this.state.data);
+        if(this.state.data.length == 0){
+            return(<div>
+                <h1>data fetching..</h1>
+            </div>);
+        }
         return(
-            form({action:"http://localhost:5001/writePosts", method:"post", encType:"multipart/form-data"},
-                input({type:"hidden", name:"pageType", value:"update"}),
-                input({type:"hidden", name:"id", value:"587064712736323e86d0a423"}),
-                input({type:"hidden", name:"weather", value:"흐림"}),
-                input({type:"file", name:"imagePath"}),
-                textarea({name:"content"}),
-                input({type:"submit", value:"수정"}),
-            )
+            <ModifyForm data={this.state.data[0]}/>
         );
     }
 }
