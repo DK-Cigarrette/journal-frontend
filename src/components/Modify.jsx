1fca2fe020d09598afde6cdb878090079d6289b2
ben.yoo <ben.yoo@kakaocorp.com>
2017-01-07 16:23:24 +0900

    test

--- src/components/Modify.jsx
+++ src/components/Modify.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
+import { render } from 'react-dom';
+
+let {
+    form,
+    input,
+    textarea
+} = React.DOM;
 
 class Modify extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        console.log("!!!!!!!!!!!!");
+        fetch("http://localhost:5001/editJournal?id=587064712736323e86d0a423", {method: "post"})
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                this.setState({
+                    data: json,
+                });
+            });
+    }
+
     render(){
+        console.log(this.state.data);
         return(
-            <h1>Modify</h1>
+            form({action:"http://localhost:5001/writePosts", method:"post", encType:"multipart/form-data"},
+                input({type:"hidden", name:"pageType", value:"update"}),
+                input({type:"hidden", name:"id", value:"587064712736323e86d0a423"}),
+                input({type:"hidden", name:"weather", value:"흐림"}),
+                input({type:"file", name:"imagePath"}),
+                textarea({name:"content"}),
+                input({type:"submit", value:"수정"}),
+            )
         );
     }
 }
+
 export default Modify
