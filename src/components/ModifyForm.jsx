04435b5a00798c6aa2ec4a32734009f0a8d8723b
scarlett.kim <scarlett.kim@kakaocorp.com>
2017-01-11 20:55:30 +0900

    (scarlett.kim) 수정창 값을 변경 못 하는 이슈로 코드 변경 value -> defaultValue 로 변경. 해야할 것 : 날씨 select select된 기본값 불러오기

--- src/components/ModifyForm.jsx
+++ src/components/ModifyForm.jsx
@@ -15,13 +15,18 @@ export default class ModifyForm extends Component {
         return (
                 form({action:`${hostURL}/editJournal`, method:"post", encType:"multipart/form-data"},
                 input({type:"hidden", name:"pageType", value:"update"}),
-                input({type:"hidden", name:"id", value:this.props.data._id}),
-                input({type:"text", name:"weather", value:this.props.data.weather}),
-                input({type:"text", name:"username", value:this.props.data.username}),
+                input({type:"hidden", name:"id", defaultValue:this.props.data._id}),
+                input({type:"text", name:"username", defaultValue:this.props.data.username}),
+                    <select id="weather">
+                        <option value="맑음">맑음</option>
+                        <option value="비">비</option>
+                        <option value="눈">눈</option>
+                        <option value="흐림">흐림</option>
+                    </select>,
                 img({src:this.props.data.imagePath}),
                 input({type:"file"}),
-                textarea({name:"content", value:this.props.data.content}),
-                input({type:"submit", value:"수정"}),
+                textarea({name:"content", defaultValue:this.props.data.content}),
+                input({type:"submit", defaultValue:"수정"}),
         ));
     }
 }
