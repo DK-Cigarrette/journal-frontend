bee71ff1fe8c503eb679de19d8f9055fb0474de1
scarlett.kim <scarlett.kim@kakaocorp.com>
2017-01-12 12:10:22 +0900

    (scarlett.kim) 수정4

--- src/components/ModifyForm.jsx
+++ src/components/ModifyForm.jsx
@@ -22,15 +22,15 @@ export default class ModifyForm extends Component {
                 form({action:`${hostURL}/writePosts`, method:"post", encType:"multipart/form-data"},
                 input({type:"hidden", name:"pageType", value:"update"}),
                 input({type:"hidden", name:"id", value:this.props.data._id}),
-                input({type:"text", name:"username", defaultValue:this.props.data.username}),
-                    <select id="weather" defaultValue={this.props.data.weather}>
+                input({type:"text", name:"username", value:this.props.data.username}),
+                    <select id="weather" name="weather" defaultValue={this.props.data.weather}>
                         <option value="맑음">맑음</option>
                         <option value="비">비</option>
                         <option value="눈">눈</option>
                         <option value="흐림">흐림</option>
                     </select>,
                 img({src:convertImgPath(this.props.data.imagePath)}),
-                input({type:"file"}),
+                input({type:"file", name:"imagePath"}),
                 textarea({name:"content", defaultValue:this.props.data.content}),
                 input({type:"submit", value:"수정"}),
         ));
